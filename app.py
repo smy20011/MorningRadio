a7892ad74bda3597e2404657572fb0a792932044
Siyuan Ma <smy20011@gmail.com>
2025-01-22 19:54:37 -0800

    Revert back to use flask as server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from feedgen.feed import FeedGenerator
-from pathlib import Path
+from flask import Flask, render_template, Response, request, send_from_directory
 import glob
 import os
 import datetime
@@ -9,37 +9,43 @@ PODCAST_TITLE = "Morning Routine"
 PODCAST_DESCRIPTION = "Programmatically Generated Podcast"
 ASSETS_DIR = "assets"  # Directory where your MP3 files are stored
 
+app = Flask(__name__)
 
+
+@app.route('/')
 def homepage():
     episodes = get_episodes()
-    env = jinja2.Environment(loader = jinja2.FileSystemLoader("templates"))
-    template = env.get_template("index.html")
-    return template.render(episodes=episodes, podcast_title=PODCAST_TITLE, podcast_description=PODCAST_DESCRIPTION)
+    return render_template("index.html", episodes=episodes, podcast_title=PODCAST_TITLE, podcast_description=PODCAST_DESCRIPTION)
 
+@app.route("/rss.xml")
 def rss_feed():
     episodes = get_episodes()
+    host = request.host
     fg = FeedGenerator()
     fg.load_extension('podcast')
     fg.title(PODCAST_TITLE)
-    fg.link(href=f"/")
+    fg.link(href=host)
     fg.description(PODCAST_TITLE)
 
     for episode in episodes:
         fe = fg.add_entry()
-        url = f"{episode['filename']}"
+        url = f"http://{host}/{episode['filename']}"
         fe.id(url)
         fe.title(f"{PODCAST_TITLE} {episode['creation_time'].strftime('%Y-%m-%d')}")
         fe.enclosure(url, 0,  'audio/mpeg')
         fe.published(episode['creation_time'])
         fe.guid(url)
 
-    return fg.rss_str(pretty=True)
+    return Response(fg.rss_str(pretty=True), mimetype="application/rss+xml")
+
+@app.route(f"/{ASSETS_DIR}/<name>")
+def podcasts(name):
+    return send_from_directory(ASSETS_DIR, name)
 
 def get_episodes():
     episodes = []
-    for file in glob.glob(f"{ASSETS_DIR}/*.mp3"):
-        stat = os.stat(file)
-        filename = os.path.basename(file)
+    for filename in glob.glob(f"{ASSETS_DIR}/*.mp3"):
+        stat = os.stat(filename)
         creation_time = datetime.datetime.fromtimestamp(stat.st_ctime, datetime.timezone.utc) 
         episodes.append({
             "filename": filename,
@@ -50,5 +56,4 @@ def get_episodes():
 
 
 if __name__ == "__main__":
-    Path(f"{ASSETS_DIR}/index.html").write_text(homepage())
-    Path(f"{ASSETS_DIR}/rss.xml").write_bytes(rss_feed())
+    app.run()
