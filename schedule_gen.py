383c38306d71a33c141b37902b3da9633f5afbbc
Siyuan Ma <smy20011@gmail.com>
2025-01-22 16:26:36 -0800

    Add more support for serving

---
+++ schedule_gen.py
@@ -0,0 +1,12 @@
+import schedule
+import time
+import os
+
+def make():
+    os.system("make")
+
+schedule.every().day.at("07:30").do(make)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
